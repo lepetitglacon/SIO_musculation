c50a55eb79c1bd735ddac5f45840decf2969ed21
gagneure <estebangagneur03@gmail.com>
2020-12-10 14:04:14 +0100

    avancée documentation sprint2

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -41,7 +41,7 @@ class RegistrationController extends AbstractController
                 $user,
                 $request,
                 $authenticator,
-                'dev' // firewall name in security.yaml
+                'main' // firewall name in security.yaml
             );
         }
 
