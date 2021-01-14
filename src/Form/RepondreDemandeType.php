9401441e45fb46203b98fa351d62e2b3df10f591
lepetitglacon <estebangagneur03@gmail.com>
2021-01-14 10:09:50 +0100

    fin envoyer une demande : debut repondre demande

---
+++ src/Form/RepondreDemandeType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Demande;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DemandeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('objet',TextType::class,[
+                "required" => true
+            ])
+            ->add('texte',TextType::class,[
+                "required" => true
+            ])
+            ->add('mail',EmailType::class,[
+                "required" => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Demande::class,
+        ]);
+    }
+}
